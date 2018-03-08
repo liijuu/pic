bd08875d121966d39c741a27314e63c18db2dfff
liijuu <1336208215@qq.com>
2018-03-08 18:08:50 +0800

    去掉白色粒子

--- can1.js
+++ can1.js
@@ -11,37 +11,44 @@ $(function () {
     ggggg = 0
 	img.src = imgs[ggggg % 7]
 
-    // if (window.DeviceMotionEvent) {
-    //     window.addEventListener('devicemotion', deviceMotionHandler)
-    // }
-    // var SHAKE_THRESHOLD = 4000
-    // var last_update = 0
-    // var x, y, z, last_x = 0, last_y = 0, last_z = 0
-    // function deviceMotionHandler(eventData) {
-    //     var acceleration =eventData.accelerationIncludingGravity;
-    //     var curTime = new Date().getTime();
-    //     if ((curTime-last_update)> 10) {
-    //         var diffTime = curTime -last_update;
-    //         last_update = curTime;
-    //         x = acceleration.x;
-    //         y = acceleration.y;
-    //         z = acceleration.z;
-    //         var speed = Math.abs(x +y + z - last_x - last_y - last_z) / diffTime * 10000;
-    //         if (speed > SHAKE_THRESHOLD) {
-    //             alert(say[ggggg % 7])
-    //             ggggg ++
-    //             img.src = imgs[ggggg % 7]
-    //         }
-    //         last_x = x;
-    //         last_y = y;
-    //         last_z = z;
-    //    }
-    // }
-    $('#canvas').click(function () {
-        alert(say[ggggg % 7])
-        ggggg ++
-        img.src = imgs[ggggg % 7]
-    })
+    if (window.DeviceMotionEvent) {
+        window.addEventListener('devicemotion', deviceMotionHandler)
+    }
+    var SHAKE_THRESHOLD = 4000
+    var last_update = 0
+    var x, y, z, last_x = 0, last_y = 0, last_z = 0
+    function deviceMotionHandler(eventData) {
+        var acceleration =eventData.accelerationIncludingGravity;
+        var curTime = new Date().getTime();
+        if ((curTime-last_update)> 10) {
+            var diffTime = curTime -last_update;
+            last_update = curTime;
+            x = acceleration.x;
+            y = acceleration.y;
+            z = acceleration.z;
+            var speed = Math.abs(x +y + z - last_x - last_y - last_z) / diffTime * 10000;
+            if (speed > SHAKE_THRESHOLD) {
+                if （window.gg）{
+                    cancelAnimationFrame(window.gg)
+                }
+                end()
+                alert(say[ggggg % 7])
+                ggggg ++
+                img.src = imgs[ggggg % 7]
+            }
+            last_x = x;
+            last_y = y;
+            last_z = z;
+       }
+    }
+    // $('#canvas').click(function () {
+    //     if （window.gg）{
+    //         cancelAnimationFrame(window.gg)
+    //     }
+    //     alert(say[ggggg % 7])
+    //     ggggg ++
+    //     img.src = imgs[ggggg % 7]
+    // })
 
 
     img.onload = function () {
